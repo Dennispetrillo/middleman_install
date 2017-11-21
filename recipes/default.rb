67ed7db05bef712516dc14f32df460679b7e6bc1
Dennis Petrillo <dennis.petrillo@yahoo.com>
2017-11-21 13:03:00 -0500

    first commit - ran update to update apt cache, installed apache2 and executed a2enmod for proxy and rewrite

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,33 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+apt_update
+apt_package 'ruby'
+apt_package 'apache2'
+
+execute 'proxy_http' do
+  command 'a2enmod proxy_http'
+end
+
+execute 'rewrite' do
+  command 'a2enmod rewrite'
+end
+
+#Install Ruby
+#apt_package 'build-essential'
+#apt_package 'libssl-dev'
+#apt_package 'libyaml-dev'
+#apt_package 'libreadline-dev'
+#apt_package 'openssl'
+#apt_package 'curl'
+#apt_package 'git-core'
+#apt_package 'zlib1g-dev'
+#apt_package 'bison'
+#apt_package 'libxml2-dev'
+#apt_package 'libxslt1-dev'
+#apt_package 'libcurl4-openssl-dev'
+#apt_package 'nodejs'
+#apt_package 'libsqlite3-dev'
+#apt_package 'sqlite3'
+
